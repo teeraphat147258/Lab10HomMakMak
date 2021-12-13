af08441cf4a0ce1a0f27fadebc1b5e593ef7ec2d
teeraphat147258 <teeraphat147258@gmail.com>
2021-12-13 16:41:45 +0700

    lab10_1 completed

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,33 +1,51 @@
 #include<iostream>
 #include<iomanip> //For using setw(), setprecision(), ...
+#include<cmath>
 using namespace std;
 
 int main(){	
+	double loan,rate,pay;
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> rate;
 	cout << "Enter amount you can pay per year: ";
+	cin >> pay;
 
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
+	int year =0;
+	double Prevbalance,Interest,Total,Payment,Newbalance;
 	cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
 	cout << setw(13) << left << "Total";
 	cout << setw(13) << left << "Payment";
 	cout << setw(13) << left << "NewBalance";
-	cout << "\n";
-	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
 	cout << "\n";	
+	Prevbalance = loan;
+
+	do{
+		Interest = (Prevbalance*rate)/100;
+		Total = Prevbalance+Interest;	
+		Payment = pay;
+		if(Payment>=Total){
+				Payment=Total;
+			}
+		Newbalance = Total-Payment;
+		year++;
+		cout << fixed << setprecision(2); 
+		cout << setw(13) << left << year; 
+		cout << setw(13) << left << Prevbalance;
+		cout << setw(13) << left << Interest;
+		cout << setw(13) << left << Total;
+		cout << setw(13) << left << Payment;
+		cout << setw(13) << left << Newbalance;
+		cout << "\n";	
+		Prevbalance = Newbalance;
+		
+
+	}while(Newbalance > 0);
+	
+	
 	
 	return 0;
 }
